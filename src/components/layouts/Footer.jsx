ee1a4c7a443eca9724a872ec4d04f43594031aaa
SadMachin3 <wizardbla78@gmail.com>
2022-12-10 11:08:32 +0530

    fix-footer-color

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
 	const footerYear = new Date().getFullYear();
 
 	return (
-		<footer className='footer p-10 bg-gray-800 text-neutral-content footer-center '>
+		<footer className='footer p-10 text-neutral-content bg-neutral footer-center '>
 			<div>
 				<GiCyberEye className='block my-2 text-4xl' />
 				<p>Copyright &copy; {footerYear}</p>
